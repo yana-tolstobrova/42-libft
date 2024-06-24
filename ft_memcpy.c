2c4a51e837d6e7d1a02651f542f51b350c2d260b
Yana Tolstobrova <ytolstob@car6s6.42barcelona.com>
2024-06-24 21:03:46 +0200

    mem functions in progress

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ytolstob <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 19:35:26 by ytolstob          #+#    #+#             */
-/*   Updated: 2024/06/24 20:30:02 by ytolstob         ###   ########.fr       */
+/*   Updated: 2024/06/24 21:01:07 by ytolstob         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,12 @@ void	*ft_memcpy(void *dest, const void *src, unsigned int n)
 
 	d = (char *)dest;
 	s = (char *)src;
-	while (n > 0 && n--)
+	if (d == NULL && s == NULL)
+		return (NULL);
+	while (n)
+	{
 		*d++ = *s++;
+		--n;
+	}
 	return (dest);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,10 +6,17 @@
 /*   By: ytolstob <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 19:47:50 by ytolstob          #+#    #+#             */
-/*   Updated: 2024/06/24 20:30:39 by ytolstob         ###   ########.fr       */
+/*   Updated: 2024/06/24 21:03:10 by ytolstob         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memmove(void *dest, const void *src, unsigned int n)
 {
+	char	*d;
+	char	*s;
+	char	*tmp;
+
+	d = (char *)dest;
+	s = (char *)src;
+
 }
